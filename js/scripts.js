3aa93a019b6c4568614f0982c176be0bf6935522
Bravehearts19 <leo18111998@gmail.com>
2021-10-07 13:04:08 +0200

    Esercitazione completata

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const nome = prompt("Inserisci il tuo nome");
+const cognome = prompt("Inserisci il tuo cognome");
+const colorePreferito = prompt("Inserisci il tuo colore preferito");
+
+alert("Inserimento dei dati completato, la tua nuova password è: " + nome + cognome + colorePreferito + "21");
